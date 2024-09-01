72f5f2e270e97ae937d722665204af4edfeb12a8
evrwaq <evrwaq@gmail.com>
2024-09-01 12:26:42 -0300

    refactor: adjust firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app"
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCtEMVpajciJGFzAnav2YbmSQCgnvTKhh8",
   authDomain: "movies-match-2998b.firebaseapp.com",
@@ -11,10 +7,8 @@ const firebaseConfig = {
   storageBucket: "movies-match-2998b.appspot.com",
   messagingSenderId: "393589199528",
   appId: "1:393589199528:web:140bd6dcec21aaba48689d"
-};
+}
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const app = initializeApp(firebaseConfig)
 
-export { app, auth }
+export { app }
