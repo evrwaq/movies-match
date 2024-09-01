619c4a1bdf617dd28d409c4f2449677e45e3462c
evrwaq <evrwaq@gmail.com>
2024-09-01 15:47:15 -0300

    refactor: remove double quotes

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,13 @@
-import { initializeApp } from "firebase/app"
+import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCtEMVpajciJGFzAnav2YbmSQCgnvTKhh8",
-  authDomain: "movies-match-2998b.firebaseapp.com",
-  projectId: "movies-match-2998b",
-  storageBucket: "movies-match-2998b.appspot.com",
-  messagingSenderId: "393589199528",
-  appId: "1:393589199528:web:140bd6dcec21aaba48689d"
+  apiKey: 'AIzaSyCtEMVpajciJGFzAnav2YbmSQCgnvTKhh8',
+  authDomain: 'movies-match-2998b.firebaseapp.com',
+  projectId: 'movies-match-2998b',
+  storageBucket: 'movies-match-2998b.appspot.com',
+  messagingSenderId: '393589199528',
+  appId: '1:393589199528:web:140bd6dcec21aaba48689d'
 }
 
 const app = initializeApp(firebaseConfig)
