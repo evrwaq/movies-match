aaf006ad3ee3f63c6172f05f35536afc42549d0b
evrwaq <evrwaq@gmail.com>
2024-09-03 23:32:17 -0300

    refactor: change firebase config to use env values

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,16 +1,16 @@
 import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCtEMVpajciJGFzAnav2YbmSQCgnvTKhh8',
-  authDomain: 'movies-match-2998b.firebaseapp.com',
-  projectId: 'movies-match-2998b',
-  storageBucket: 'movies-match-2998b.appspot.com',
-  messagingSenderId: '393589199528',
-  appId: '1:393589199528:web:140bd6dcec21aaba48689d'
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
-const db = getFirestore(app);
+const db = getFirestore(app)
 
 export { app, db }
