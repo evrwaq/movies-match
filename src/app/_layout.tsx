fbfe2706b8e7959fbdf7af985ace05d4e02b80a6
evrwaq <evrwaq@gmail.com>
2024-09-05 23:20:15 -0300

    feat: add RootLayout with base stack navigation

---
+++ src/app/_layout.tsx
@@ -0,0 +1,32 @@
+import { useFonts } from 'expo-font'
+import { Slot, Stack } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect } from 'react'
+import 'react-native-reanimated'
+
+SplashScreen.preventAutoHideAsync()
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  })
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded, error])
+
+  if (!loaded && !error) {
+    return <Slot />
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="home" options={{ headerShown: false }} />
+      <Stack.Screen name="login" options={{ headerShown: false }} />
+      {/* <Stack.Screen name="+not-found" /> */}
+    </Stack>
+  )
+}
