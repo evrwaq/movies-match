64cd569b5ce795d06a1917e49af81a0000b3d83e
evrwaq <evrwaq@gmail.com>
2024-09-05 23:21:42 -0300

    feat: add InitialPageHandler with base router navigate

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,29 @@
+import { Link, useRouter } from 'expo-router'
+import { useEffect } from 'react'
 import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-export default function Page() {
+export default function InitialPageHandler() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.navigate('/home')
+    }, 2400)
+  }, [router]);
+  // useEffect(() => {
+  //   const a = false;
+  //   if (a) {
+  //     router.replace('/login')
+  //   } else {
+  //     router.replace('/home')
+  //   }
+  // }, [router])
+
   return (
-    <Text>Home</Text>
+    <SafeAreaView>
+      <Text>Initial Page</Text>
+      <Link replace href='/login'>Login</Link>
+    </SafeAreaView>
   )
 }
