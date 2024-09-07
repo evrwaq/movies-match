b73adfcd667af90f9d7acb13222648f828a59df5
evrwaq <evrwaq@gmail.com>
2024-09-07 19:00:00 -0300

    feat: add useAuthentication hook with signIn and signOut methods

---
+++ src/hooks/useAuthentication.ts
@@ -0,0 +1,59 @@
+import {
+  GoogleSignin,
+  isErrorWithCode,
+  statusCodes,
+} from '@react-native-google-signin/google-signin'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useFirebase } from './useFirebase'
+import { User } from '../models'
+
+export function useAuthentication() {
+  const { addUser } = useFirebase()
+
+  const signIn = async () => {
+    try {
+      GoogleSignin.configure()
+      await GoogleSignin.hasPlayServices()
+      const userInfo = await GoogleSignin.signIn()
+      const user: User = {
+        id: userInfo.data!.user.id,
+        email: userInfo.data!.user.email,
+        name: userInfo.data!.user.name!,
+      }
+      await AsyncStorage.setItem('user', JSON.stringify(user))
+      await addUser(user)
+    } catch (error) {
+      if (isErrorWithCode(error)) {
+        switch (error.code) {
+          case statusCodes.SIGN_IN_CANCELLED:
+            // user cancelled the login flow
+            break
+          case statusCodes.IN_PROGRESS:
+            // operation (eg. sign in) already in progress
+            break
+          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+            // play services not available or outdated
+            break
+          default:
+          // some other error happened
+        }
+      } else {
+        // an error that's not related to google sign in occurred
+      }
+    }
+  }
+
+  const signOut = async () => {
+    try {
+      await GoogleSignin.signOut()
+      // setState({ user: null }); // Remember to remove the user from your app's state as well
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return {
+    signIn,
+    signOut,
+  }
+}
