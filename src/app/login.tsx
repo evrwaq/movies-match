c37850c155c467945d333a5dc14458359bc3ad09
evrwaq <evrwaq@gmail.com>
2024-09-07 19:47:30 -0300

    chore: add google services files

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,10 +1,21 @@
-import { Text } from 'react-native'
+import { Button, StyleSheet, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useAuthentication } from '../hooks/useAuthentication'
 
 export default function LoginScreen() {
+  const { signIn } = useAuthentication()
+
   return (
-    <SafeAreaView>
-      <Text>Login</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={{ color: 'white' }}>Login</Text>
+      <Button title="Login with Google" onPress={signIn} />
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
+  },
+})
