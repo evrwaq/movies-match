0c0d348936a4ec6256fdadf8de1b6cb8b07ccf0a
evrwaq <evrwaq@gmail.com>
2024-09-08 00:14:58 -0300

    chore: adjust metro config

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,6 @@
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
 const { getDefaultConfig } = require('@expo/metro-config')
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
+// eslint-disable-next-line no-undef
 const defaultConfig = getDefaultConfig(__dirname)
 defaultConfig.resolver.sourceExts.push('cjs')
 
