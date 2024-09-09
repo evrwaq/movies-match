1ee4a7449310b90d0e7e2ce787271b8e97acfa21
evrwaq <evrwaq@gmail.com>
2024-09-09 00:17:49 -0300

    style: add base style to login screen

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,7 +1,7 @@
-import { Button, StyleSheet, Text } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useAuthentication } from '../hooks/useAuthentication'
-import { StatusBar } from 'expo-status-bar'
 
 export default function LoginScreen() {
   const { signIn } = useAuthentication()
@@ -9,8 +9,16 @@ export default function LoginScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="light" />
-      <Text style={{ color: 'white' }}>Login</Text>
-      <Button title="Login with Google" onPress={signIn} />
+
+      <View style={styles.topContainer}>
+        <Text style={styles.logoTitle}>Movies Match</Text>
+      </View>
+
+      <View style={styles.bottomContainer}>
+        <Pressable style={styles.button} onPress={signIn}>
+          <Text style={styles.buttonText}>Login with Google</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   )
 }
@@ -19,5 +27,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#121212',
+    padding: 15,
+  },
+  topContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bottomContainer: {
+    flex: 1,
+  },
+  logoTitle: {
+    color: '#F56700',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    fontSize: 48,
+  },
+  button: {
+    backgroundColor: '#F56700',
+    margin: 15,
+    padding: 15,
+    borderRadius: 4,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: '700',
+    color: 'white',
+    textAlign: 'center',
   },
 })
