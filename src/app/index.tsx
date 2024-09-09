b16fdf847c5464437f5840258c8b47b89d84a509
evrwaq <evrwaq@gmail.com>
2024-09-08 23:38:43 -0300

    feat: add method to get user current data and redirect to screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,31 +1,21 @@
-import { Link, useRouter } from 'expo-router'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useRouter } from 'expo-router'
 import { useEffect } from 'react'
-import { Text } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function InitialPageHandler() {
   const router = useRouter()
 
   useEffect(() => {
-    setTimeout(() => {
-      router.navigate('/home')
-    }, 2400)
+    const getUserData = async () => {
+      const userData = await AsyncStorage.getItem('user')
+      if (userData) {
+        router.replace('/home')
+      } else {
+        router.replace('/login')
+      }
+    }
+    getUserData()
   }, [router])
-  // useEffect(() => {
-  //   const a = false;
-  //   if (a) {
-  //     router.replace('/login')
-  //   } else {
-  //     router.replace('/home')
-  //   }
-  // }, [router])
 
-  return (
-    <SafeAreaView>
-      <Text>Initial Page</Text>
-      <Link replace href="/login">
-        Login
-      </Link>
-    </SafeAreaView>
-  )
+  return null
 }
