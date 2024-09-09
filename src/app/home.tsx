efbad5bf3a1060ebf14624eb173315b89a2bb290
evrwaq <evrwaq@gmail.com>
2024-09-09 00:07:50 -0300

    feat: add clean async storage to home debug

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,10 +1,14 @@
-import { Text } from 'react-native'
+import { Button, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useAuthentication } from '../hooks/useAuthentication'
 
 export default function HomeScreen() {
+  const { cleanAsyncStorage } = useAuthentication()
+
   return (
     <SafeAreaView>
       <Text>Home</Text>
+      <Button title="Clean Async Storage" onPress={cleanAsyncStorage} />
     </SafeAreaView>
   )
 }
